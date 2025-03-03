8010117590db138b8b5e9615723277d188e0ea61
ddd <ddd>
2025-03-03 22:48:48 +0500

    settings added

--- src/core/settings.py
+++ src/core/settings.py
@@ -5,9 +5,43 @@ from pydantic import(
 )
 from pydantic_settings import BaseSettings
 
+from core.app_logging import logging
+
+logger = logging.getLogger("SETTINGS")
+
+
+class AppSettings(BaseSettings):
+    ...
+
 
 class PgSettings(BaseSettings):
+    POSTGRES_DRV: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
     POSTGRES_URL: PostgresDsn | None = None
 
+    @field_validator("POSTGRES_URL")
+    def pg_url_validate(
+        cls,
+        v,
+        values: ValidationInfo,
+    ) -> PostgresDsn:
+        data = values.data
+        url_data = {
+            "scheme": data.get("POSTGRES_DRV"),
+            "username": data.get("POSTGRES_USER"),
+            "password": data.get("POSTGRES_PASSWORD"),
+            "host": data.get("POSTGRES_SERVER"),
+            "port": data.get("POSTGRES_PORT"),
+            "path": data.get("POSTGRES_DB"),
+        }
+        return PostgresDsn.build(**url_data)
+    
 
+appsettings = AppSettings()
+logger.info("AppStiings created")
 pgsettings = PgSettings()
+logger.info("PgSettings created")
